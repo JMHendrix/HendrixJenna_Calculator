0be686c7d2f703acc8a564b03787da07dbc1a763
Jenna Hendrix <JMHendrix@student.fullsail.edu>
2022-06-10 16:16:30 -0400

    Created layout with buttons and text control.

--- HendrixJenna_Calculator/Main.h
+++ HendrixJenna_Calculator/Main.h
@@ -9,5 +9,33 @@ public:
 
 public:
 
+//numeric buttons
+	wxButton* buttonOne;
+	wxButton* buttonTwo;
+	wxButton* buttonThree;
+	wxButton* buttonFour;
+	wxButton* buttonFive;
+	wxButton* buttonSix;
+	wxButton* buttonSeven;
+	wxButton* buttonEight;
+	wxButton* buttonNine;
+	wxButton* buttonZero;	
+	wxButton* buttonDecimal;
+	wxButton* buttonNegative;
+
+	wxButton* buttonAdd;
+	wxButton* buttonSubtract;
+	wxButton* buttonMultiply;
+	wxButton* buttonDivide;
+	wxButton* buttonBinary;
+	wxButton* buttonHex;
+	wxButton* buttonModulo;
+	wxButton* buttonClear;
+	wxButton* buttonEqual;
+
+	wxTextCtrl* answerBox;
+
+
+	void OnButtonClicked();
 };
 
